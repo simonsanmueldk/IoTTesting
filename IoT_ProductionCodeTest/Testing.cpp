fc912f1d1b18837086fba2862a257535901f97e3
simonsanmueldk <simonsamuelhol@gmail.com>
2022-05-20 11:41:16 +0200

    New testing for temp hum, and changes in testing structure

--- IoT_ProductionCodeTest/Testing.cpp
+++ IoT_ProductionCodeTest/Testing.cpp
@@ -1,4 +1,10 @@
-//#include "pch.h"
+/*
+ * Testing.cpp
+ *
+ * Created: 18/05/2022 12.49.21
+ *  Author: Simon Samuel & Chen
+ */
+
 #include "gtest/gtest.h"
 #include "C:\Users\simon\New SEP\Testing\fff-master\fff.h"
 //#include "C:\Users\simon\New SEP\Testing\VIAFreeRTOSTest\FreeRTOS_FFF_MocksDeclaration.h"
@@ -20,27 +26,31 @@ float humidity;
 #include "C:\Users\simon\New SEP\IOT\4.Semester\drivers\hih8120.h"
 
 }
-//Mocks 
+//IoT mocks 
 FAKE_VOID_FUNC(tempHum_init);
-FAKE_VOID_FUNC(tempHum_taskRun);
+FAKE_VOID_FUNC(tempHum_getDataFromTempHumSensorTask);
 FAKE_VALUE_FUNC(float, hih8120_getHumidity);
 FAKE_VALUE_FUNC(float, hih8120_getTemperature);
 FAKE_VALUE_FUNC(hih8120_driverReturnCode_t, hih8120_wakeup);
 
-
-
-
+//FreeRTOS mocks
 FAKE_VOID_FUNC(vTaskDelay, TickType_t);
 FAKE_VALUE_FUNC(BaseType_t, xSempahoreGive, SemaphoreHandle_t);
 FAKE_VOID_FUNC(xTaskDelayUntil, TickType_t*, TickType_t);
 
-//------------------------
+//Co2 Mocks
 
+//DonwlinkHandler Mocks
+
+//UplinkHandler Mocks
+
+
+//IOT Setup
 class TempHumTaskTest : public ::testing::Test {
 protected:
 	void SetUp() override {
 		RESET_FAKE(tempHum_init);
-		RESET_FAKE(tempHum_taskRun);
+		RESET_FAKE(tempHum_getDataFromTempHumSensorTask);
 		RESET_FAKE(vTaskDelay);
 		RESET_FAKE(xTaskDelayUntil);
 		RESET_FAKE(hih8120_getHumidity);
@@ -52,68 +62,71 @@ protected:
 	void TearDown() override {}
 };
 
-//------------------------------------------------------
+//Co2 Setup
 
-TEST_F(TempHumTaskTest, tempHum_init_calledDuringInitialisation) {
-	tempHum_init();
-	EXPECT_EQ(1, tempHum_init_fake.call_count);
-}
+//DonwlinkHandler Setup
 
+//UplinkHandler Setup
+
+
+
+//IoT Testing
+// 
+//First test
 TEST_F(TempHumTaskTest, FirstTest)
 {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
 }
 
+//Initalisation correct test
+TEST_F(TempHumTaskTest, tempHum_init_calledDuringInitialisation) {
+	tempHum_init();
+	EXPECT_EQ(1, tempHum_init_fake.call_count);
+}
+
+//Get humidity test
 TEST_F(TempHumTaskTest, TempHum_getHumididty_success) {
 	//Arrange
 	hih8120_getHumidity_fake.return_val = 0.0;
 	//Act
-	tempHum_taskRun();
+	hih8120_getHumidity();
 	//Assert
-	EXPECT_EQ(0.0, hih8120_getHumidity_fake.call_count);
 	EXPECT_EQ(0.0, humidity);
 }
 
-
+//Get temperature test
 TEST_F(TempHumTaskTest, TempHum_getTemperature_success) {
 	//Arrange
 	hih8120_getTemperature_fake.return_val = 0.0;
 	//Act
-	tempHum_taskRun();
+	hih8120_getTemperature();
 	//Assert
-	EXPECT_EQ(0.0, hih8120_getTemperature_fake.call_count);
 	EXPECT_EQ(0.0, temperature);
 }
 
-
-TEST_F(TempHumTaskTest, TempHum_xTaskDelayUntil_calledCorrectly) {
+//Get data from sensor test
+TEST_F(TempHumTaskTest, TempHum_GetDataFromSensor_Succes) {
 	//Arrange
+	hih8120_getHumidity_fake.return_val = 0.0;
+	hih8120_getTemperature_fake.return_val = 0.0;
 	//Act
-	tempHum_taskRun();
+	tempHum_getDataFromTempHumSensorTask();
 	//Assert
-	EXPECT_EQ(1, xTaskDelayUntil_fake.call_count);
-	EXPECT_TRUE(NULL != xTaskDelayUntil_fake.arg0_val);
-}
-
-TEST_F(TempHumTaskTest, TempHum_vTaskDelay_calledCorrectly) {
-	//Arrange
-	//Act
-	tempHum_taskRun();
-	//Assert
-	EXPECT_EQ(2, vTaskDelay_fake.call_count);
-	EXPECT_EQ(pdMS_TO_TICKS(50), vTaskDelay_fake.arg0_history[0]);
-	EXPECT_EQ(pdMS_TO_TICKS(500), vTaskDelay_fake.arg0_history[1]);
+	EXPECT_EQ(humidity, hih8120_getHumidity_fake.call_count);
+	EXPECT_EQ(temperature, hih8120_getTemperature_fake.call_count);
 }
 
 
-TEST_F(TempHumTaskTest, TempHum_driver_wakeup_success) {
+//Create task test
+TEST_F(TempHumTaskTest, TempHUm_taskCreated_succes) {
 	//Arrange
-	hih8120_wakeup_fake.return_val = HIH8120_OK;
+	
 	//Act
-	tempHum_taskRun();
+	
 	//Assert
-	EXPECT_EQ(1, hih8120_wakeup_fake.call_count);
-	EXPECT_EQ(HIH8120_OK, hih8120_wakeup_fake.return_val);
+
 }
+
+
 
