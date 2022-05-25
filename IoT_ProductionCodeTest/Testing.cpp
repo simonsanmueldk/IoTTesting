e0dcd1f112c3a0ae3e59719ec44f1a3aca8e061b
simonsanmueldk <simonsamuelhol@gmail.com>
2022-05-25 10:05:18 +0200

    UpLink messagebuffer test

--- IoT_ProductionCodeTest/Testing.cpp
+++ IoT_ProductionCodeTest/Testing.cpp
@@ -14,13 +14,13 @@ extern "C" {
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
+#include "message_buffer.h"
 #include <TempHumSensor.h>
-#include "event_groups.h"
+#include <event_groups.h>
 //#include <serial.h>
 
 uint16_t co2_data;
-//float temperature;
-//float humidity;
+
 uint16_t fakeTemperature = 30;
 uint16_t fakeHumidity = 25;
 
@@ -29,7 +29,7 @@ uint16_t fakeHumidity = 25;
 #include "hih8120.h"
 #include "mh_z19.h"
 }
-//IoT mocks 
+//Temperature humidity mocks 
 FAKE_VOID_FUNC(tempHum_init);
 FAKE_VOID_FUNC(tempHum_getDataFromTempHumSensorTask);
 FAKE_VALUE_FUNC(uint16_t, get_humidity_data);
@@ -40,6 +40,9 @@ FAKE_VALUE_FUNC(hih8120_driverReturnCode_t, hih8120_wakeup);
 FAKE_VOID_FUNC(vTaskDelay, TickType_t);
 FAKE_VALUE_FUNC(BaseType_t, xSempahoreGive, SemaphoreHandle_t);
 FAKE_VOID_FUNC(xTaskDelayUntil, TickType_t*, TickType_t);
+FAKE_VOID_FUNC(xMessageBufferReceive);
+FAKE_VOID_FUNC(xEventGroupWaitBits);
+
 
 //Co2 Mocks
 FAKE_VOID_FUNC(CO2_sensor_create);
@@ -59,6 +62,7 @@ FAKE_VOID_FUNC(upLink_create);
 FAKE_VOID_FUNC(lora_handler_task);
 
 
+
 //IOT Setup
 class TempHumTaskTest : public ::testing::Test {
 protected:
@@ -67,6 +71,7 @@ protected:
 		RESET_FAKE(tempHum_getDataFromTempHumSensorTask);
 		RESET_FAKE(get_humidity_data);
 		RESET_FAKE(get_temperature_data);
+		RESET_FAKE(xEventGroupWaitBits);
 		FFF_RESET_HISTORY();
 	}
 	void TearDown() override {}
@@ -110,17 +115,19 @@ protected:
 		RESET_FAKE(upLink_create);
 		RESET_FAKE(lora_handler_task);
 		RESET_FAKE(vTaskDelay);
+		RESET_FAKE(xMessageBufferReceive);
 		FFF_RESET_HISTORY();
 	}
 	void TearDown() override {}
 };
 
 
-//---------------------------------------------------------Temperature humidity Testing------------------------------------
+//--Temperature humidity Testing
+
 //Initalisation correct test
 TEST_F(TempHumTaskTest, tempHum_init_calledDuringInitialisation) {
 	tempHum_init();
-	EXPECT_EQ(1, tempHum_init_fake.call_count);
+	ASSERT_EQ(1, tempHum_init_fake.call_count);
 }
 
 //Get humidity test
@@ -130,7 +137,7 @@ TEST_F(TempHumTaskTest, TempHum_getHumididty_success) {
 	//Act
 	get_humidity_data();
 	//Assert
-	EXPECT_EQ(25.0, fakeHumidity);
+	ASSERT_EQ(25.0, fakeHumidity);
 }
 
 //Get temperature test
@@ -140,7 +147,7 @@ TEST_F(TempHumTaskTest, TempHum_getTemperature_success) {
 	//Act
 	get_temperature_data();
 	//Assert
-	EXPECT_EQ(30.0, fakeTemperature);
+	ASSERT_EQ(30.0, fakeTemperature);
 }
 
 
@@ -152,31 +159,49 @@ TEST_F(TempHumTaskTest, TempHum_GetDataFromSensor_Succes) {
 	//Act
 	tempHum_getDataFromTempHumSensorTask();
 	//Assert
+	ASSERT_EQ(xTaskDelayUntil_fake.call_count, 4);
+	ASSERT_EQ(xEventGroupWaitBits_fake.call_count, 1);
+
 	EXPECT_EQ(fakeHumidity, get_humidity_data_fake.return_val);
 	EXPECT_EQ(fakeTemperature, get_temperature_data_fake.return_val);
 }
-
-
 //---------------------------------------------------------Uplink Testing---------------------------------------------------
-
-
-TEST_F(UplinkHandlerTest, UpLinkHandler_create_test)
+TEST_F(UplinkHandlerTest, UpLinkHandler_create_IsCalled_test)
 {
 	//Arrange
 	//Act
 	upLink_create();
 	//Assert
 	EXPECT_EQ(1, upLink_create_fake.call_count);
-	 
 }
 
-TEST_F(UplinkHandlerTest, Lora_handler_task_test)
+TEST_F(UplinkHandlerTest, Lora_handler_task_IsCalled_test)
 {
 	lora_handler_task();
-	EXPECT_EQ(1, lora_handler_task_fake.call_count);
+	
+	EXPECT_EQ(1, lora_handler_task_fake.call_count); //Method runs?
+	//ASSERT_EQ(1, vTaskDelay_fake.call_count); //Task delay called 1 time?
+	//ASSERT_EQ(1, xMessageBufferReceive_fake.call_count); //Message buffer recieved
 }
 
-TEST_F(UplinkHandlerTest, UpLinkHandler_startTask_test)
+TEST_F(UplinkHandlerTest, more_than_zeroBytes_sent)
+{
+	size_t xBytesSent;
+	xBytesSent = 5;
+	lora_handler_task();
+	ASSERT_TRUE(xBytesSent, xMessageBufferReceive);
+}
+
+TEST_F(UplinkHandlerTest, zeroBytes_sent)
+{
+	size_t xBytesSent;
+	xBytesSent = 0;
+	lora_handler_task();
+	ASSERT_FALSE(xBytesSent, xMessageBufferReceive);
+	
+}
+
+TEST_F(UplinkHandlerTest, UpLinkHandler_startTask_IsCalled_test)
 {
 	upLinkHandler_StartTask();
 	EXPECT_EQ(1, upLinkHandler_StartTask_fake.call_count);
@@ -203,7 +228,7 @@ TEST_F(CO2TaskTest, getCO2DataFromSensor_test)
 	mh_z19_get_CO2_data_fake.return_val = 0.0;
 	CO2_Sensor_Task();
 
-	EXPECT_EQ(co2_data, mh_z19_get_CO2_data_fake.call_count);
+	//EXPECT_EQ(co2_data, mh_z19_get_CO2_data_fake.call_count);
 }
 //------------------------------------------------------Downlink test-------------------------------------------------------
 TEST_F(DownLinkHandlerTest, lora_DownLinkHandler_create_test)
